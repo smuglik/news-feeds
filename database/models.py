768f73fa7ce8c8e8fe587e858d97714d30d22e1b
Vladislav Smuglienko <vladislav.smuglienko@t-systems.com>
2023-05-28 20:29:54 +0200

    refactor migrations

--- database/models.py
+++ database/models.py
@@ -1,21 +1,22 @@
+from datetime import UTC, datetime
 import uuid
-from datetime import datetime
 
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from database.base import Base
 
-
 class User(Base):
     __tablename__ = "user"  # noqa
 
-    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
+    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4)  # noqa
     first_name: Mapped[str] = mapped_column(nullable=False)
     last_name: Mapped[str] = mapped_column(nullable=False)
     email: Mapped[str] = mapped_column(nullable=False, unique=True)
     is_superuser: Mapped[bool] = mapped_column(default=False, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
 
+    posts: Mapped[list["Post"]] = relationship(back_populates="author")
     def __repr__(self) -> str:
         return self.username
 
@@ -23,8 +24,11 @@ class User(Base):
 class Post(Base):
     __tablename__ = "post"  # noqa
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True)  # noqa
     title: Mapped[str] = mapped_column(nullable=False)
     body: Mapped[str] = mapped_column(nullable=False)
-    author: Mapped[User] = relationship(User)
-    created: Mapped[datetime] = mapped_column(default=datetime.now())
+    author_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    author: Mapped["User"] = relationship(back_populates="posts")
+
+
+created: Mapped[datetime] = mapped_column(default=datetime.now(tz=UTC))
