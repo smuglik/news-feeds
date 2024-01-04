397e6a38b8a66e85ec92e0868421e845ef342ae9
Vladislav Smuglienko <smuglik26@yandex.ru>
2024-01-04 21:49:01 +0300

    fixes

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -1,9 +1,8 @@
 from fastapi import APIRouter
 
-from api.endpoints import auth, feeds, products
+from api.endpoints import auth, feeds
 
 routes = APIRouter()
 routes.include_router(feeds.routes, tags=['feeds'])
 routes.include_router(auth.routes, tags=['authentication'])
-routes.include_router(products.routes, tags=['products'])
 
