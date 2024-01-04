4c7dc0f9f6b8142c60158a8823685518ba61483e
Vladislav Smuglienko <smuglik26@yandex.ru>
2024-01-04 21:48:48 +0300

    updates

--- backend/helpers/populate_data.py
+++ backend/helpers/populate_data.py
@@ -4,8 +4,9 @@ import sys
 from mimesis import Schema, Field
 from mimesis.locales import Locale
 
-from database.models import Product
-from database.sessions import SessionLocal, redis_connection
+from api.security import get_hashed_password
+from database.models import User
+from database.sessions import SessionLocal
 
 
 async def main() -> None:
@@ -16,28 +17,23 @@ async def main() -> None:
     f = Field(Locale.EN)
     schema = Schema(
         schema=lambda: {
-            "name": f("finance.company"),
-            "description": f("text.text", quantity=10),
-            "cost": f("finance.price", minimum=10, maximum=3000),
-            "image": {
-                "id": f("uuid"),
-                "data": f("image", )
-            }
+            "id": f("uuid"),
+            "first_name": f("person.first_name"),
+            "last_name": f("person.last_name"),
+            "email": f("person.email"),
+            "is_superuser": f("boolean"),
+            "password": get_hashed_password("1")
         },
-        iterations=20
+        iterations=3
     )
     data = schema.create()
     async with SessionLocal() as session:
-        session.add_all([Product(
-            name=item["name"],
-            description=item["description"],
-            cost=item["cost"],
-            image_id=item["image"]["id"]
-        ) for item in data])
+        session.add_all([User(**item) for item in data])
         await session.commit()
 
 
 if __name__ == '__main__':
+
     if sys.platform == "win32":
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     asyncio.run(main())
